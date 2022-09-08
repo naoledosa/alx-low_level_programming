74eafb93b77cd3fb7af0d14e8faefb0cff32f479
naoledosa <naoledosa@gmail.com>
2022-09-08 14:10:11 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,6 @@ int main(void)
 	/*
 	 * main function
 	 */
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
