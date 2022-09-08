87315f7b54da738b5cd4e35a878cbee957707928
naoledosa <naoledosa@gmail.com>
2022-09-08 15:02:34 -0700

    lets try

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-
 /*
- *   main - Entry point
  *
- *   prints Programming is like building a multilingual puzzle 
+ *    main - Entry point 
  *
- *   Return: Always 0 (Success)
+ *    Description: 'prints Programming is like building a multilingual puzzle' 
+ *
+ *    Return: Always 0 (Success)
+ *
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+       	puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
-
