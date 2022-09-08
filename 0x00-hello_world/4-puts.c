4363d54f5b3be3b20a6a02808475265ddc9e5be4
naoledosa <naoledosa@gmail.com>
2022-09-08 14:38:20 -0700

    do

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-
-
 /*
  *
  *   main - Entry point
@@ -10,11 +8,9 @@
  *   Return: Always 0 (Success)
  *
  */
-
 int main(void)
 
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
