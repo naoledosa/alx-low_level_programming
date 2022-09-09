8bd4101ea3744b1f7fd8e6534da1fec2bb2a3eb9
naoledosa <naoledosa@gmail.com>
2022-09-09 07:24:07 -0700

    fixed fixed fixed

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,26 +1,16 @@
 /* headers - predifned libraries */
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <time.h>
-
-/*
- *	This program generates random number
- *
- *	and assigns the number to n
- *
- *	based on the value of n
- *
- *	decides whether it is positive, negative or zero
- *
- *	and prints message accordingly
- *
+/**
+ *This program generates random number and assigns the number to n
+ *based on the value of n
+ *decides whether it is positive, negative or zero
+ *and prints message accordingly
  */
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
