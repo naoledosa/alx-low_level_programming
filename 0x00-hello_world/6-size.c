ac0a27446e7da8c1d187349c4a57ac8f274ed8df
naoledosa <naoledosa@gmail.com>
2022-09-12 05:01:34 -0700

    size of fixed done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - returns the size of different data types
- *        based on the operating system
+ * main - Prints the size of various types based on
+ *	  the computer it is compiled and run on..
  *
  * Return - always (0) -  Success
  */
@@ -17,7 +17,7 @@ int main(void)
 	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(longIntType));
-	printf("Size of a long long int: %zu byte(s)\n",sizeof(longLongIntType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(longLongIntType));
 	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 
 	return (0);
