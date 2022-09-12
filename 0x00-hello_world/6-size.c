ea1929c0201ba9c1795dbe444442c94959df0c3f
naoledosa <naoledosa@gmail.com>
2022-09-12 04:13:18 -0700

    size of fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ int main(void)
 	float floatType;
 	double doubleType;
 
-	printf("Size of a char: %zu byte(s)\n", charType);
-	printf("Size of an int: %zu byte(s)\n", intType);
-	printf("Size of a float: %zu byte(s)\n", floatType);
-	printf("Size of a double: %zu byte(s)\n", doubleType);
+	printf("Size of a char: %c byte(s)\n", charType);
+	printf("Size of an int: %d byte(s)\n", intType);
+	printf("Size of a float: %f byte(s)\n", floatType);
+	printf("Size of a double: %lf byte(s)\n", doubleType);
 
 	return (0);
 }
