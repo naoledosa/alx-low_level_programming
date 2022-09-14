8a488d08999f8cf134f58cafd4b124403c941c52
naoledosa <naoledosa@gmail.com>
2022-09-14 07:14:28 -0700

    putchar solved

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet, in lowercase, followed by a new line
+ *
+ * Return: always 0.
+ */
+
+int main(void)
+{
+	print_alphabet();
+
+	return (0);
+}
