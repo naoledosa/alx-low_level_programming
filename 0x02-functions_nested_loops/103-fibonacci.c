6d61f0f3d1d02d3533a467ec38dbbfec385b3409
naoledosa <naoledosa@gmail.com>
2022-09-14 15:18:48 -0700

    fibonacci add

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of the even-valued terms
+ *
+ * Return: always 0.
+ */
+
+int main(void)
+{
+	unsigned long x = 0, y = 1, sum;
+	float total_sum;
+
+	while (1)
+	{
+		sum = x + y;
+
+		if (sum > 4000000)
+			break;
+
+		if((sum % 2) == 0)
+			total_sum += sum;
+
+		x = y;
+		y = sum;
+
+	}
+
+	printf("%.0f\n", total_sum);
+
+	return (0);
+}
+
+
+
