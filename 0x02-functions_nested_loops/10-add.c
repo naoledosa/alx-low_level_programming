cd06fa0de256810c0f5a59138ea54315d1d042a7
naoledosa <naoledosa@gmail.com>
2022-09-14 13:58:38 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - adds two integers and returns the result
+ * @x: number being tested
+ * @y: number being tested
+ * Return: always 0.
+ */
+
+int add(int x, int y)
+{
+	int sum = 0;
+
+	sum = x + y;
+
+	return sum;
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,6 +12,6 @@ int _abs(int val);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-
+int add(int, int);
 
 #endif /* _MAIN_H_  */
