5587ee1468ca868a6802c1cae6518ce7b6cffe00
naoledosa <naoledosa@gmail.com>
2022-09-14 13:14:48 -0700

    8 to 24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer, 
+ *              starting from 00:00 to 23:59
+ * Return: always 0.
+ */
+
+void jack_bauer(void)
+{
+	int min;
+	int day;
+
+	for (day = 0; day <= 23; day++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			if (day < 10 && min < 10)
+				_putchar('0' + day + ':' + '0' + min);
+			else if (day < 10 && min > 10)
+				_putchar('0' + day + ':' + min);
+			else if (day > 10 && min < 10)
+				_putchar(day + ':' + '0' + min);
+			else
+				_putchar(day + ':' + min);
+		}
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,6 +10,7 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int val);
 int print_last_digit(int);
+void jack_bauer(void);
 
 
 #endif /* _MAIN_H_  */
