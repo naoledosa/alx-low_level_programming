7e3c32f88980bed066aa3983bca707c3f1e5d2be
naoledosa <naoledosa@gmail.com>
2022-09-14 07:28:18 -0700

    right

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-#include "_putchar.h"
 #include "main.h"
 
 /**
@@ -10,16 +9,7 @@
 int main(void)
 {
 
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
+	printf("_putchar\n");
 
 	return (0);
 }
