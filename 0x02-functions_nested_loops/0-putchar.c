d1f2b14e2d3be330ca8821bff2ac3d105b2170c8
naoledosa <naoledosa@gmail.com>
2022-09-14 06:45:20 -0700

    finally fixed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,7 +9,16 @@
 int main(void)
 {
 
-	printf("_putchar\n");
+	printf("_");
+	printf("p");
+	printf("u");
+	printf("t");
+	printf("c");
+	printf("h");
+	printf("a");
+	printf("r");
+	printf("\n");
+
 
 	return (0);
 }
