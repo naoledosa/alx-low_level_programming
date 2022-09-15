583ddf3c7be5c0c59b5aeea1fce0d5275757c55e
naoledosa <naoledosa@gmail.com>
2022-09-15 05:01:08 -0700

    5-more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,8 +2,6 @@
 
 /**
  * more_numbers - prints 10 times the numbers, from 0 to 14
- *
- * Return: always 0.
  */
  
 void more_numbers(void)
@@ -15,7 +13,9 @@ void more_numbers(void)
 		for (number = 0; number <= 14; number++)
 		{
 			if (number > 9)
+			{
 				_putchar((number / 10) + '0');
+			}
 			_putchar((number % 10) + '0');
 		}
 		_putchar('\n');
