6f711f37b39322ae32f1cacd1c54fbf2e69b10f2
naoledosa <naoledosa@gmail.com>
2022-09-15 08:52:38 -0700

    f

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,16 +2,17 @@
 
 /**
  * main - tests function that prints if integer is positive or negative
- *
  * Return: 0
  */
 
 int main(void)
 {
+	
 	int number;
 
 	number = 0;
 	positive_or_negative(number);
 
 	return (0);
+
 }
