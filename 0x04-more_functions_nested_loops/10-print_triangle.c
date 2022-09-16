ba1f66471a8eebc9a0f6eadc2c55be1d83cd07bf
naoledosa <naoledosa@gmail.com>
2022-09-16 04:28:31 -0700

    fixed

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,6 +3,7 @@
 /**
  * print_triangle - prints a triangle, followed by a new line
  * @size: size of traiangle
+ * Return: always 0.
  */
 
  void print_triangle(int size)
@@ -13,21 +14,21 @@
 	{
 		for (i = 1; i <= size; i++)
 		{
+
 			for (j = size - i; j > 0; j--)
-			{
 				_putchar(' ');
-			}
-			
+
 			for (j = 0; j < i; j++)
-			{
 				_putchar('#');
-			}
-			
+
+
 			if (i == size)
 				continue;
-			
+
+
 			_putchar('\n');
 		}
+	
 	}
 
 	_putchar('\n');
