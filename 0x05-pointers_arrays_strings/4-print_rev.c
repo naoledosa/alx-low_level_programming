92af6968134f5e03610aa8f29283b93674739607
naoledosa <naoledosa@gmail.com>
2022-09-20 04:51:15 -0700

    fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,13 +9,13 @@ void print_rev(char *s)
 {
 	int length = 0, i;
 
-	while (s[i++])
+	while (*(s + 1))
 		length++;
 
 	if (length != 0)
 	{
 		for (i = length - 1; i >= 0; i--)
-			_putchar(s[i]);
+			_putchar(*(s + 1));
 		_putchar('\n');
 	}
 }
