90660931b697e4c85562737c269c90dead5ec332
naoledosa <naoledosa@gmail.com>
2022-09-21 13:18:37 -0700

    magic fixed

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -7,17 +7,11 @@
 
 int main(void)
 {
-	int n;
-	int a[5];
 	int *p;
 
-	a[2] = 1024;
+	*(p + 2) = 98;
 
-	p = &n;
-
-	*(p + 5) = 98;
-
-	printf("a[2] = %d\n", a[2]);
+	printf("a[2] = %d\n", *(p + 2));
 
 	return (0);
 }
