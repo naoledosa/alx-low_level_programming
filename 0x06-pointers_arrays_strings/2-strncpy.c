4be882b966479f578995f3b683aa58c13021ab7a
naoledosa <naoledosa@gmail.com>
2022-09-21 11:11:36 -0700

    buffer fixed

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,19 +10,17 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0, j = 0;
-
-	while (dest[i++])
-		j++;
+	int i;
 
 	for (i = 0; src[i] != '\0' && i < n; i++)
 	{
 		dest[i] = src[i];
 	}
 
-	for (i = j; i < n; i++)
+	while (i < n)
 	{
 		dest[i] = '\0';
+		i++;
 	}
 	
 	return (dest);
