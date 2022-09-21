f3b8cce41ea5cc1e054b41b1f1634c58fd263dcb
naoledosa <naoledosa@gmail.com>
2022-09-21 03:46:05 -0700

    lower to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @s: string
+ * Return: always 0.
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i++])
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] -= 32;
+		}
+
+		i++;
+	}
+
+	return (s);
+}
+
