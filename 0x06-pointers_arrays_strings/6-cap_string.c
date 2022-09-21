0a309d4c6a96ef3c8d7d4e2576ad5ad316a0f3f5
naoledosa <naoledosa@gmail.com>
2022-09-21 12:21:38 -0700

    hot fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,4 @@
-#include "main"
+#include "main.h"
 
 /**
  * cap_string - capitalizes all words of a string.
@@ -18,16 +18,22 @@ char *cap_string(char *a)
 				a[i] -= 32;
 
 			if (a[i - 1] == 9 || a[i - 1] == 10 ||
-			    a[i - 1] == 32 || a[i - 1] == 33)
+					a[i - 1] == 32 || a[i - 1] == 33)
+			{
 				a[i] -= 32;
+			}
 
 			if (a[i - 1] == 40 || a[i - 1] == 41 ||
-			    a[i - 1] == 44 || a[i - 1] == 46)
+					a[i - 1] == 44 || a[i - 1] == 46)
+			{
 				a[i] -= 32;
+			}
 
 			if (a[i - 1] == 59 || a[i - 1] == 123 ||
-			    a[i - 1] == 125)
+					a[i - 1] == 125)
+			{
 				a[i] -= 32;
+			}
 		}
 	}
 
