36c07c4badcace52280f7474647083611fd85728
naoledosa <naoledosa@gmail.com>
2022-09-21 13:26:16 -0700

    cp

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,17 +10,19 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int i = 0, j = 0;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	while (src[i++])
+		j++;
+
+	for (i = 0; src[i] && i < n; i++)
 	{
 		dest[i] = src[i];
 	}
 
-	while (i < n)
+	for (i = j; i < n; i++)
 	{
 		dest[i] = '\0';
-		i++;
 	}
 	
 	return (dest);
