93bc57a32ec74a54a950a2e37c17fe503d5d1811
naoledosa <naoledosa@gmail.com>
2022-09-21 03:22:25 -0700

    compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings.
+ * @s1: pointer to char
+ * @s2: pointer to char
+ * Return: always 0.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while ((*s1 && *s2) && (*s1 == *s2))
+	{
+		*s1++;
+		*s2++;
+	}
+
+	return (*s1 - *s2);
+}
