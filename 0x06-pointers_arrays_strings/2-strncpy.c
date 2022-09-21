443d167e2062c6c4b98d9951eb0ccb092ca0c48e
naoledosa <naoledosa@gmail.com>
2022-09-21 03:15:32 -0700

    string copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: pointer destination
+ * @src: pointer source
+ * @n: number being tested
+ * Return: always 0.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while (dest[i++])
+		j++;
+
+	for (i = 0; src[i] && i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for (i = j; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	
+	return (dest);
+}
