19a666dfd630225a5aee898f1a58eb59de020c7f
naoledosa <naoledosa@gmail.com>
2022-09-21 12:15:22 -0700

    fix

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,7 +10,7 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *s);
-char *cap_string(char *s);
+char *cap_string(char *a);
 char *leet(char *s);
 char *rot13(char *);
 void print_number(int n);
