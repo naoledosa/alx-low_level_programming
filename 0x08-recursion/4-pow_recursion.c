85190c83d47d75f46b45a44f84b6b53a63c50150
naoledosa <naoledosa@gmail.com>
2022-09-28 12:21:10 -0700

    power fix

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -12,10 +12,10 @@ int _pow_recursion(int x, int y)
 	int result = x;
 
 	if (y < 0)
-		return(-1);
+		return (-1);
 
 	else if (y == 0)
-		return(1);
+		return (1);
 
 	result *= _pow_recursion(x, y - 1);
 
