f41d45dab8b64dd2a4c5c729895957a11e387947
naoledosa <naoledosa@gmail.com>
2022-09-28 14:14:33 -0700

    palindrome done

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,56 +1,61 @@
 #include "main.h"
 
-int check(char *s);
+
+int length(char *s);
+int check(char *s, int i, int j);
+int is_palindrome(char *s);
 
 /**
- * length - returns length of string
- * @s: string to be tested
+ * length - Returns the length of a string.
+ * @s: string
  * Return: length
  */
 
 int length(char *s)
 {
-	if (*s == '\0')
-		return (0);
+	int i = 0;
 
-	s++;
-	return (length(s) + 1);
+	if (*(s + i))
+	{
+		i++;
+		i += length(s + i);
+	}
+	return (i);
 }
 
 /**
- * check - checks if string is palindrome
- * @s: string to be tested
+ * check - Checks if a string is a palindrome.
+ * @s: string
+ * @i: lenght
+ * @j: index
  * Return: 0 or 1.
  */
 
-int check(char *s)
+
+int check(char *s, int i, int j)
 {
-	int i = length(s) - 1;
+	if (s[j] == s[i / 2])
+		return (1);
 
-	if (*s == s[i])
-	{
-		s++;
-		i--;
-	}
-	else
-	{
-		return (0);
-	}
+	if (s[j] == s[i - j - 1])
+		return (check(s, i, j + 1));
 
-	return (1);
+	return (0);
 }
 
 /**
- * is_palindrome - returns 1 if a string is a palindrome and 0 if not.
- * @s: string to be tested
- * Return: 1 0r 0.
+ * is_palindrome - Checks if a string is a palindrome.
+ * @s: string
+ * Return: 0 or 1.
  */
 
 int is_palindrome(char *s)
 {
-	if (*s == '0')
+	int j = 0;
+	int i = length(s);
+
+	if (!(*s))
 		return (1);
 
-	return (check(s));
+	return (check(s, i, j));
 }
-
