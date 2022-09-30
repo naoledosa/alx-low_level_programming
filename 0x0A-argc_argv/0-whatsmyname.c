b48e20613562a38a9a20b8c2f4b3bf4c79458565
naoledosa <naoledosa@gmail.com>
2022-09-30 06:00:00 -0700

    my name fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,11 +3,14 @@
 
 /**
  * main - prints its name, followed by a new line.
- * Return: its own name.
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: always 0.
  */
 
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
+
 	return (0);
 }
