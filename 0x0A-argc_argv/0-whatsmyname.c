30959bb5b227b46e2fbe2c9bc9ced4318e0836a3
naoledosa <naoledosa@gmail.com>
2022-09-30 05:57:18 -0700

    my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints its name, followed by a new line.
+ * Return: its own name.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+0-whatsmyname.c - prints its name, followed by a new line.
+
