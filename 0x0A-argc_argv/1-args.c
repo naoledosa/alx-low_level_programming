a83fbc09cce00423b8e672d533ba0874f99d2906
naoledosa <naoledosa@gmail.com>
2022-09-30 06:11:02 -0700

    number of arguments fixed

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,9 +7,9 @@
  * Return: always 0.
  */
 
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
