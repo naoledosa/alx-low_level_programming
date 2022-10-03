8b1df4c95f58c7b17685ab4949194b666813561b
naoledosa <naoledosa@gmail.com>
2022-10-03 12:12:12 -0700

    two D array fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,14 +16,14 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	array = malloc((sizeof(int *) * height);
+	array = malloc((sizeof(int *)) * height);
 	
 	if (array == NULL)
 		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
-		array[i] = malloc((sizeof(int) * width);
+		array[i] = malloc((sizeof(int)) * width);
 
 		if (array[i] == NULL)
 		{
