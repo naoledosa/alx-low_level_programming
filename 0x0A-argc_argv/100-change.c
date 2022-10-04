ca94103bb4bd259d39071a64aa58f3d0badc2289
naoledosa <naoledosa@gmail.com>
2022-10-04 04:53:42 -0700

    change final

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -17,23 +17,28 @@ int main(int argc, char *argv[])
 	{
 		cents = atoi(*(argv + 1));
 
-		while (cents > 0)
+		if (cents == 0)
 		{
-			if (cents >= 25)
-				cents -= 25;
-			else if (cents >= 10)
-				cents -= 10;
-			else if (cents >= 5)
-				cents -= 5;
-			else if (cents >= 2)
-				cents -= 2;
-			else
-				cents -= 1;
-			
-			coins++;
+			printf("0\n");
 		}
-
-		return (printf("%d\n", coins), 0);
+		else
+		{
+			while (cents > 0)
+			{
+				if (cents >= 25)
+					cents -= 25;
+				else if (cents >= 10)
+					cents -= 10;
+				else if (cents >= 5)
+					cents -= 5;
+				else if (cents >= 2)
+					cents -= 2;
+				else
+					cents -= 1;
+				coins++;
+			}
+			return (printf("%d\n", coins), 0);
+		}
 	}
 	else
 	{
