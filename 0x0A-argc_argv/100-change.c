d203435e3f9b74801ce4bdf1ea94b5bc9ca62a33
naoledosa <naoledosa@gmail.com>
2022-10-04 04:42:55 -0700

    change works

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 /**
  * main - prints the minimum number of coins to
@@ -14,43 +13,32 @@ int main(int argc, char *argv[])
 {
 	int cents, coins = 0;
 
-	if (argc != 2)
+	if (argc == 2)
 	{
-		printf("Error\n");
-		return (1);
+		cents = atoi(*(argv + 1));
+
+		while (cents > 0)
+		{
+			if (cents >= 25)
+				cents -= 25;
+			else if (cents >= 10)
+				cents -= 10;
+			else if (cents >= 5)
+				cents -= 5;
+			else if (cents >= 2)
+				cents -= 2;
+			else
+				cents -= 1;
+			
+			coins++;
+		}
+
+		return (printf("%d\n", coins), 0);
 	}
-
-	cents = atoi(argv[1]);
-
-	while (cents > 0)
+	else
 	{
-		coins++;
-		if ((cents - 25) >= 0)
-		{
-			cents -= 25;
-			continue;
-		}
-
-		if ((cents - 10) >= 0)
-		{
-			cents -= 10;
-			continue;
-		}
-		
-		if ((cents - 5) >= 0)
-		{
-			cents -= 5;
-			continue;
-		}
-		
-		if ((cents - 2) >= 0)
-		{
-			cents -= 2;
-			continue;
-		}
-		cents--;
+		return (printf("Error\n"), 1);
 	}
-	printf("%d\n", coins);
-
+	
 	return (0);
 }
