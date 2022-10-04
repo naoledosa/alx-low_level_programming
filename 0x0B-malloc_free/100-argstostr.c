f2274655e513d4e8191b042b40eb2dca3ff0ed97
naoledosa <naoledosa@gmail.com>
2022-10-03 23:58:59 -0700

    argument

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * argstostr - concatenates all the arguments of your program.
+ * @ac: argument count
+ * @av: argument vector
+ * Return: NULL or argument
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int a = 0, b = 0, c = 0, d = 0;
+	char *s;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	while (b < ac)
+	{
+		while (av[b][c])
+		{
+			a++;
+			c++;
+		}
+
+		c = 0;
+		b++;
+	}
+
+	s = malloc((sizeof(char) * a) + ac + 1);
+
+	b = 0;
+
+	while (av[b])
+	{
+		while (av[b][c])
+		{
+			s[d] = av[b][c];
+			d++;
+			j++;
+		}
+
+		s[d] = '\n';
+
+		j = 0;
+		d++;
+		b++;
+	}
+
+	d++;
+	s[d] = '\0';
+	return (s);
+}
+
