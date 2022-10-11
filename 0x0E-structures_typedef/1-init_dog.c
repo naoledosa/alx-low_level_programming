bedfa33ed64ee1ad54ba74e35ee4b2facba8d9b8
naoledosa <naoledosa@gmail.com>
2022-10-10 23:23:55 -0700

    initialize dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,11 +2,11 @@
 #include "dog.h"
 
 /**
- * init_dog - Initializes a dog structure
+ * init_dog - Initializes a dog structure.
  * @d: A dog structure.
  * @name: The name of the dog.
  * @age: The age of the dog.
- * @owner: The owner of the dog..
+ * @owner: The owner of the dog.
  * Return: Nothing
  */
 
